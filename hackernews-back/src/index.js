730d18e8a0c81452442783fbf94d1c1a9ac0080a
Akilesh Rao <akileshrao19@gmail.com>
2019-11-07 21:09:52 +0530

    Added subscription and login signup functionality

--- hackernews-back/src/index.js
+++ hackernews-back/src/index.js
@@ -1,53 +1,28 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('./generated/prisma-client');
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription');
 
 const resolvers = {
-  Query: {
-    info: () => `This is an API of Hackernews Clone`,
-    feed: (root, args, context, info) => {
-      return context.prisma.links();
-    },
-    link: (root, args) => links.find(link => link.id === args.id)
-  },
-
-  Mutation: {
-    post: (root, args, context) => {
-      return context.prisma.createLink({
-        url: args.url,
-        description: args.description
-      })
-    },
-
-    updateLink: (root, args) => {
-      const link = {
-        id: args.id,
-        description: args.description,
-        url: args.url
-      };
-
-      let linkIndex = links.indexOf(links.find(link => link.id === args.id));
-
-      links[linkIndex] = link;
-
-      return link;
-    },
-
-    deleteLink: (root, args) => {
-      let linkRef = links.find(link => link.id === args.id);
-      links.map(link => {
-        if (link.id === args.id) {
-          links.splice(links.indexOf(link), 1);
-        }
-      });
-      return linkRef;
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link,
+  Subscription
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { prisma }
-})
+  context: request => {
+    return {
+      ...request,
+      prisma
+    }
+  }
+});
 
 server.start(() => console.log("Server is running on localhost 4000")); 
--- hackernews-back/src/resolvers/Mutation.js
+++ hackernews-back/src/resolvers/Mutation.js
@@ -1,12 +1,12 @@
-// post = (root, args, context) => {
-//   return context.prisma.createLink({
-//     url: args.url,
-//     description: args.description
-//   })
-// }
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { APP_SECRET, getUserId } = require('../utils');
 
 
-let signup = async (parent, args, context, info) => {
+
+// ------ signup
+let signup = async (root, args, context, info) => {
   const password = await bcrypt.hash(args.password, 10);
   const user = await context.prisma.createUser({ ...args, password });
   const token = jwt.sign({ userId: user.id }, APP_SECRET);
@@ -17,10 +17,49 @@ let signup = async (parent, args, context, info) => {
   }
 }
 
-let login = async (parent, args, context, info) => {
-  const user = await 
+
+// ------ login
+let login = async (root, args, context, info) => {
+  const user = await context.prisma.user({ email: args.email });
+  if (!user) {
+    throw new Error('User not found!');
+  }
+
+  const valid = await bcrypt.compare(args.password, user.password);
+  if (!valid) {
+    throw new Error("Incorrect Password!");
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user
+  }
+}
+
+
+// ------ posts
+let post = async (root, args, context, info) => {
+  const userId = getUserId(context);
+  return await context.prisma.createLink({
+    url: args.url,
+    description: args.description,
+    postedBy: { connect: { id: userId } }
+  })
+}
+
+
+let vote = async (root, args, context, info) => {
+  const userId = getUserId(context);
+  const linkExists = await context.prisma.$exists.vote({
+    user: { id: userId },
+    link : { id : args.linkId}
+  })
 }
 
 module.exports = {
+  signup,
+  login,
   post
 }
